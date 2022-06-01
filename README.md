6fc62d2e1757555cb17117a221c426846add9481
Jame Tacker <james.tacker@saucelabs.com>
2022-06-01 15:48:00 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Sauce Algolia Demo
+
+_This project was generated with [parcel](https://wesbos.com/javascript/14-es-modules-and-structuring-larger-apps/81-bundling-and-building-with-parcel)._
+
+## Get started
+
+To run this project locally, install the dependencies and run the local server:
+
+```sh
+npm install
+npm start
+```
---
+++ src/app.js
@@ -0,0 +1,78 @@
+/* global instantsearch */
+
+import { hitTemplate } from "./helpers";
+
+const search = instantsearch({
+    appId: "RO95H65NEO",
+    apiKey: "eb980e41546c08d3e422562074fa0380",
+    indexName: "crawler_help_center",
+    searchParameters: {
+        hitsPerPage: 5,
+        attributesToSnippet: ["description:24"],
+        snippetEllipsisText: " [...]"
+    }
+});
+
+// Uncomment the following widget to add hits list.
+
+search.addWidget(
+    instantsearch.widgets.hits({
+        container: "#hits",
+        templates: {
+            empty: "No results.",
+            item: function (hit) {
+                return hitTemplate(hit);
+            }
+        }
+    })
+);
+
+// Uncomment the following widget to add a search bar.
+
+search.addWidget(
+    instantsearch.widgets.searchBox({
+        container: "#searchbox",
+        placeholder: "Search for products",
+        autofocus: false
+    })
+);
+
+// Uncomment the following widget to add search stats.
+
+search.addWidget(
+    instantsearch.widgets.stats({
+        container: "#stats",
+        templates: {
+            body(hit) {
+                return `<span role="img" aria-label="emoji">⚡️</span> <strong>${
+                    hit.nbHits
+                }</strong> results found ${
+                    hit.query != "" ? `for <strong>"${hit.query}"</strong>` : ``
+                } in <strong>${hit.processingTimeMS}ms</strong>`;
+            }
+        }
+    })
+);
+
+// Uncomment the following widget to add categories list.
+
+search.addWidget(
+    instantsearch.widgets.refinementList({
+        container: "#categories",
+        attributeName: "type",
+        autoHideContainer: false,
+        templates: {
+            header: "Categories"
+        }
+    })
+);
+
+// Uncomment the following widget to add pagination.
+
+search.addWidget(
+    instantsearch.widgets.pagination({
+        container: "#pagination"
+    })
+);
+
+search.start();
