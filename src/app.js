dd8a0b871bb3a2fa2008e2c985deb08b7f23a76d
discombobulateme <paloma.oliveira@saucelabs.com>
2022-06-14 16:07:50 +0200

    Fix hooks

--- src/app.js
+++ src/app.js
@@ -1,27 +1,31 @@
 import React from 'react';
 import algoliasearch from 'algoliasearch/lite';
-import { InstantSearch, SearchBox, Hits, Highlight } from 'react-instantsearch-hooks-web';
+import { Breadcrumb, Configure, InstantSearch, SearchBox, Hits, Highlight, Pagination } from 'react-instantsearch-hooks-web';
 
 const searchClient = algoliasearch('RO95H65NEO', '8d249abc4671e5554fe8f451ffa5db50');
 
 function Hit({ hit }) {
-  return (
-    <article>
-      <img src={hit.image} alt={hit.name} />
-      <p>{hit.categories[0]}</p>
-      <h1>
-        <Highlight attribute='name' hit={hit} />
-      </h1>
-      <p>${hit.price}</p>
-    </article>
-  );
+    console.log(JSON.stringify(hit));
+    return (
+      <article>
+        <h1>
+          <Highlight attribute='name' hit={hit} />
+        </h1>
+        <p>{hit.description}</p>
+        <p>{hit.url}</p>
+        <p>{hit.type}</p>
+      </article>
+    );
 }
 
 function App() {
   return (
     <InstantSearch searchClient={searchClient} indexName='crawler_help_center'>
-      <h1>Hello App</h1>
+      <Configure hitsPerPage={20} />
       <SearchBox />
+      <Hits hitComponent={Hit} />
+      <Breadcrumb attributes={['categories.lvl0', 'categories.lvl1', 'categories.lvl2']} />
+      <Pagination />
     </InstantSearch>
   );
 }
