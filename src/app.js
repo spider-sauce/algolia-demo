35ef4e5d66a77ba20772b4c56f171bbb026b8d7f
discombobulateme <paloma.oliveira@saucelabs.com>
2022-06-15 15:07:08 +0200

    Rearrange app with Hit as separate component

--- src/app.js
+++ src/app.js
@@ -1,29 +1,17 @@
 import React from 'react';
 import algoliasearch from 'algoliasearch/lite';
-import { Breadcrumb, Configure, InstantSearch, SearchBox, Hits, Highlight, Pagination } from 'react-instantsearch-hooks-web';
+import { Breadcrumb, Configure, InstantSearch, SearchBox, Hits, Pagination } from 'react-instantsearch-hooks-web';
+
+import Hit from './components/hit.js';
 
 const searchClient = algoliasearch('RO95H65NEO', '8d249abc4671e5554fe8f451ffa5db50');
 /**
  * add snipet to return smaller description
  * fix breadcrumbs
- * fix pagination, now with bullets
  * fix results returning as a numbered list
- * add side bar wuth preview
+ * add side bar with preview
  * add highlight in the description
  */
-function Hit({ hit }) {
-    console.log(JSON.stringify(hit));
-    return (
-      <article>
-        <h1>
-          <Highlight attribute='name' hit={hit} />
-        </h1>
-        <p>{hit.description}</p>
-        <p>{hit.url}</p>
-        <p>{hit.type}</p>
-      </article>
-    );
-}
 
 function App() {
   return (
@@ -31,7 +19,7 @@ function App() {
       <Configure hitsPerPage={20} />
       <SearchBox />
       <Hits hitComponent={Hit} />
-      <Breadcrumb attributes={['categories.lvl0', 'categories.lvl1', 'categories.lvl2']} />
+      <Breadcrumb attributes={['']} />
       <Pagination />
     </InstantSearch>
   );
