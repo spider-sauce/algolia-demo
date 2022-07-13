08f34af935d86589459267275e8f127ca47a5c63
discombobulateme <paloma.oliveira@saucelabs.com>
2022-07-13 17:31:33 +0200

    Add Stats

--- src/app.js
+++ src/app.js
@@ -1,10 +1,22 @@
-import React from 'react';
-import algoliasearch from 'algoliasearch/lite';
-import { Breadcrumb, Configure, InstantSearch, SearchBox, Hits, Pagination } from 'react-instantsearch-hooks-web';
+import React from 'react'
+import algoliasearch from 'algoliasearch/lite'
+import {
+    Breadcrumb,
+    Configure,
+    Hits,
+    InstantSearch,
+    Pagination,
+    SearchBox,
+} from 'react-instantsearch-hooks-web'
 
-import Hit from './components/hit.js';
+import {Stats} from 'react-instantsearch-dom'
 
-const searchClient = algoliasearch('RO95H65NEO', '8d249abc4671e5554fe8f451ffa5db50');
+import Hit from './components/hit.js'
+
+const searchClient = algoliasearch(
+    'RO95H65NEO',
+    '8d249abc4671e5554fe8f451ffa5db50',
+)
 /**
  * add snipet to return smaller description
  * fix breadcrumbs
@@ -13,26 +25,42 @@ const searchClient = algoliasearch('RO95H65NEO', '8d249abc4671e5554fe8f451ffa5db
  * add highlight in the description
  */
 
-const results = algoliasearch
+// const Stats = (nbHits) => <p>{nbHits ? `${nbHits} results` : 'No results'}</p>
 
 function App() {
-  console.log(results);
-  return (
-    <InstantSearch searchClient={searchClient} indexName='crawler_help_center'>
-      <header>
-        <Configure hitsPerPage={15} />
-        <SearchBox placeholder='What are you searching for?' />
-      </header>
-      <main>
-        <Hits hitComponent={Hit} />
-        <Breadcrumb
-          attributes={['breadcrumbs.lvl0', 'breadcrumbs.lvl1', 'breadcrumbs.lvl2']}
-        />
-        <div></div>
-        <Pagination />
-      </main>
-    </InstantSearch>
-  );
+    return (
+        <InstantSearch
+            searchClient={searchClient}
+            indexName="crawler_help_center">
+            <header>
+                <Configure hitsPerPage={15} />
+                <SearchBox placeholder="What are you searching for?" />
+            </header>
+            <main>
+                <Hits hitComponent={Hit} />
+                <Breadcrumb
+                    attributes={[
+                        'breadcrumbs.lvl0',
+                        'breadcrumbs.lvl1',
+                        'breadcrumbs.lvl2',
+                    ]}
+                    separator={'>'}
+                />
+                <div>
+                    <Stats
+                        translations={{
+                            stats(nbHits, processingTimeMS) {
+                                return nbHits
+                                    ? `${nbHits.toLocaleString()} results found in ${processingTimeMS.toLocaleString()}ms`
+                                    : 'No results'
+                            },
+                        }}
+                    />
+                    <Pagination />
+                </div>
+            </main>
+        </InstantSearch>
+    )
 }
 
-export default App;
+export default App
